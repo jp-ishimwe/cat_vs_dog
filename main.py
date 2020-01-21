5c6a46b314e773649e9878255994d1a3e70d5e77
ISHIMWE_Jean-Paul <jishimwe@aimsammi.org>
2020-01-21 21:39:20 +0000

    Added model train and test

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset
+from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 import os
 import glob
@@ -16,7 +17,7 @@ from PIL import Image
 from os import listdir
 from model import CNN
 from dataset import DatasetLoader
-
+from helper import matplotlib_imshow, images_to_probs, plot_classes_preds
 
 
 #Fill free alter the n_features and lr initializations
@@ -38,31 +39,89 @@ test_d = DatasetLoader(data_root + '/test', transform=transformation)
 train_loader = torch.utils.data.DataLoader(train_d, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_d, batch_size=64, shuffle=True)
 
-accuracy_list = []
+#Available classes
+classes = ['cat','dog']
 
+# get some random training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+# create grid of images
+img_grid = torchvision.utils.make_grid(images)
+# show images
+matplotlib_imshow(img_grid, one_channel=False)
+
+#Initialize and write to tensorboard
+writer = SummaryWriter('runs/experiment1')
+writer.add_image('sixty_four_cats_dogs_images', img_grid)
+
+criterion = nn.CrossEntropyLoss()
+model = CNN(input_size, n_features, output_size)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+#Adding the model to tensorboard
+writer.add_graph(model, images)
+writer.close()
+
+#uncomment to see class prediction
+#plot_classes_preds(model, images, labels)
+
+accuracy_list = []
 def train(epoch, model, perm=torch.arange(0, 784).long()):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        
+        data, target = data.to(device), target.to(device)
+
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-            
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+            # ...log the running loss
+            writer.add_scalar('training loss',
+                              loss / 100,
+                              epoch * len(train_loader) + batch_idx)
+
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals',
+                              plot_classes_preds(model, data, labels),
+                              global_step=epoch * len(train_loader) + batch_idx)
+            loss = 0.0
+
+
 def test(model, perm=torch.arange(0, 784).long()):
     model.eval()
     test_loss = 0
     correct = 0
-    for data, target in test_loader:
+    for batch_idx, (data, target) in enumerate(test_loader):
+
+        data, target = data.to(device), target.to(device)
+
         output = model(data)
-        test_loss += F.nll_loss(output, target, reduction='sum').item()                                                               
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability                                                                 
+        # loss = criterion(output, target)
+        test_loss += criterion(output,
+                               target).item()  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[
+            1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+
+        if batch_idx % 100 == 0:
+            # ...log the running loss
+            writer.add_scalar('test loss',
+                              test_loss / 1000,
+                              epoch * len(test_loader) + batch_idx)
+
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals',
+                              plot_classes_preds(model, data, labels),
+                              global_step=epoch * len(test_loader) + batch_idx)
+            test_loss = 0.0
 
     test_loss /= len(test_loader.dataset)
     accuracy = 100. * correct / len(test_loader.dataset)
@@ -70,15 +129,11 @@ def test(model, perm=torch.arange(0, 784).long()):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         accuracy))
-
     
 if __name__ == "__main__":
 
-    model_cnn = CNN(input_size, n_features, output_size)
-    optimizer = optim.SGD(model_cnn.parameters(), lr=0.01, momentum=0.5)
-    
-    for epoch in range(0, 1):
-	train(epoch, model_cnn)
-	test(model_cnn)
-    
-    
+    device = torch.device('cuda')
+    model = model.to(device)
+    for epoch in range(15):
+        train(epoch, model)
+        test(model)
